41317df517eaf8c19b2ba7996bd45f97deb8f346
nguyenthanhlam21 <lamn47593@gmail.com>
2024-05-26 14:54:16 +0700

    commit

---
+++ spotify/src/components/DisplayHome.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import Navbar from './Navbar'
+import { albumsData } from '../assets/assets'
+import AlbumItem from './AlbumItem'
+import { songsData } from '../assets/assets'
+import SongItem from './SongItem'
+
+const DisplayHome = () => {
+  return (
+    <div>
+        <Navbar/>
+        <div className='my-5 font-bold text-2x1'>
+            <h1 className='my-5 font-bold text-2x1'>Featured Charts</h1>
+            <div className='flex overflow-auto'>
+            {albumsData.map((item, index)=>(<AlbumItem key={index} name={item.name} desc={item.desc}  id={item.id} image={item.image}/>))}
+            </div>
+        </div>
+        <div className='my-5 font-bold text-2x1'>
+            <h1 className='my-5 font-bold text-2x1'>Today's Biggest Hit</h1>
+            <div className='flex overflow-auto'>
+            {songsData.map((item, index)=>(<SongItem key={index} name={item.name} desc={item.desc}  id={item.id} image={item.image}/>  ))}
+            </div>
+        </div>
+        
+    </div>
+  )
+}
+
+export default DisplayHome
